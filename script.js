a2a77b2785b2796cc4e6d53a17e0f6ca22c26247
Rayson N Mendes <raysonmendes2.0@gmail.com>
2023-03-29 14:29:52 -0300

    adicao da marcacao dos itens de menu ativos

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const fetchSvg = (image) => {
-  // console.log("Função fetchSvg rodou aqui!");
   fetch(image.src)
     .then((response) => response.text())
     .then((response) => {
@@ -10,3 +9,32 @@ const fetchSvg = (image) => {
       return true;
     });
 };
+
+const listLi = document.getElementsByClassName("main-nav-li");
+
+// Observar elementos na tela
+focusDiv = "";
+const observer = new IntersectionObserver(
+  (entriers) => {
+    Array.from(entriers).forEach((entry) => {
+      if ((entry.intersectionRatio >= 0.8) & (entry.intersectionRatio <= 1)) {
+        entry.target.classList.add("active");
+
+        focusDiv = "#" + entry.target.parentElement.id;
+
+        for (let index = 0; index < listLi.length; index++) {
+          if (listLi[index].children[0].getAttribute("href") == focusDiv) {
+            listLi[index].classList.add("active");
+          } else {
+            listLi[index].classList.remove("active");
+          }
+        }
+      } else {
+        entry.target.classList.remove("active");
+      }
+    });
+  },
+  {
+    threshold: 0.8,
+  }
+);
