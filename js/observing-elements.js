264bf09883b741d1d4362a249148ce2ff1c1ce86
Rayson N Mendes <raysonmendes2.0@gmail.com>
2023-03-30 01:03:21 -0300

    resolvido bug de mostrar secao errada mobile

--- js/observing-elements.js
+++ js/observing-elements.js
@@ -1,44 +1,37 @@
-const listLi = document.getElementsByClassName("main-nav-li");
-
 // Observar elementos na tela
-focusDiv = "";
+
 const observer = new IntersectionObserver(
   (entriers) => {
     Array.from(entriers).forEach((entry) => {
-      //   if ((entry.intersectionRatio >= 0.8) & (entry.intersectionRatio <= 1)) {
-      if (entry.intersectionRatio == 1) {
+      if (entry.intersectionRatio >= 0.7) {
         entry.target.classList.add("active");
-
-        focusDiv = "#" + entry.target.parentElement.id;
+        enableItemLi("#" + entry.target.parentElement.id);
       } else {
         entry.target.classList.remove("active");
+        disableItemLi("#" + entry.target.parentElement.id);
       }
     });
-    this.onload(activeItemLi());
-    console.log(focusDiv);
   },
   {
-    threshold: 1,
+    threshold: 0.7,
   }
 );
 
-function activeItemLi() {
-  for (let index = 0; index < listLi.length; index++) {
-    if (listLi[index].children[0].getAttribute("href") !== focusDiv) {
-      listLi[index].classList.remove("active");
-      console.log(
-        "Removeu a classe active de " +
-          listLi[index].children[0].getAttribute("href")
-      );
-    } else {
-      listLi[index].classList.add("active");
-      console.log(
-        "Adicionou a classe active de " +
-          listLi[index].children[0].getAttribute("href")
-      );
+function enableItemLi(idDiv) {
+  Array.from(document.querySelectorAll(".main-nav-li")).forEach((navLi) => {
+    if (navLi.children[0].getAttribute("href") == idDiv) {
+      navLi.classList.add("active");
     }
-  }
+  });
 }
+function disableItemLi(idDiv) {
+  Array.from(document.querySelectorAll(".main-nav-li")).forEach((navLi) => {
+    if (navLi.children[0].getAttribute("href") == idDiv) {
+      navLi.classList.remove("active");
+    }
+  });
+}
+
 onload = function () {
   Array.from(document.querySelectorAll(".section")).forEach((element) => {
     observer.observe(element);
